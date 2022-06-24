e0622b7269005faf2048fa17f4437b7b7999ce82
kwonmory <kwonmory@gmail.com>
2022-06-24 16:47:01 +0900

    chore: 환경 세팅

---
+++ README.md
@@ -0,0 +1,11 @@
+![logo](https://avatars.githubusercontent.com/u/12936720?v=4)
+
+# morykwon Utils
+
+A collection of Util functions used by morykwon
+
+# Todo
+
+[] export type
+[] build-file optimization
+[] npm deploy
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  target: ['node'],
+  mode: 'production',
+  devtool: 'source-map',
+  entry: path.resolve(__dirname, './src/index.ts'),
+  optimization: {
+    minimize: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.ts', '.js'],
+        },
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  output: {
+    library: '_',
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, './dist'),
+    filename: 'utils.min.js',
+  },
+};
